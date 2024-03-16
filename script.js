8a9ef05b928376b24e4e8b6adb21c46fb559ca15
Divyamourya04 <116952899+Divyamourya04@users.noreply.github.com>
2024-03-16 20:34:36 +0530

    first commit and

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let count=0;
 document.getElementById("message");
 document.getElementById("decrement-btn").onclick=function name(params) {
-    count-=1;
+    
    
-    if(count<0) {
+    if(count<=0) {
         
         console.log("Error:cannot go below 0");
         document.getElementById("message").innerHTML="Error:cannot go below 0";
        
     } 
     else {
-        
+        count-=1;
         document.getElementById("countlabel").innerHTML=count; 
         console.log(" ");
         message.classList.remove('hide');
